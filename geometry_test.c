8676af6e92b89eded2259e6bf12da84ad7025649
chakrya ros <chakryaros@cu-engr2-2-1-10.201.64.28.int.colorado.edu>
2018-11-29 11:38:40 -0700

    add test

--- geometry_test.c
+++ geometry_test.c
@@ -145,6 +145,21 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+START_TEST(test_2d_area_traingle)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+    
+    a.x = 6;
+    a.y = 6;
+    b.x = 0;
+    b.y = 0;
+    c.x = 7;
+    c.y = 2;
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 15);
+}
+END_TEST
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
